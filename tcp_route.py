cd53ed0068d56acef5cab6930a750a9f079e883b
Jonas B <jonas.bystrom@hm.com>
2017-01-23 18:41:29 +0100

    Got http proxy routing working.

--- tcp_route.py
+++ tcp_route.py
@@ -3,26 +3,11 @@
 from base64 import b64encode
 from os import environ
 from random import uniform
-from sched import scheduler
 from select import select
 import socket
-from threading import Semaphore, Thread
+from threading import Thread
+from time import sleep
 
-
-class EternalScheduler:
-	def __init__(self):
-		self.sched = scheduler()
-		self.sem = Semaphore(0)
-		Thread(target=self.run).start()
-
-	def run(self):
-		while True:
-			self.sched.run()
-			self.sem.acquire()
-
-	def enter(self, delay, action, argument=()):
-		self.sched.enter(delay, 1, action, argument)
-		self.sem.release()
 
 def readline(s):
 	data = b = ''
@@ -36,7 +21,6 @@ def http_proxy_connect(addr):
 	try:
 		proxy = environ['http_proxy']
 	except Exception as e:
-		print(e)
 		s = socket.socket()
 		s.connect(addr)
 		return s, 0, {}
@@ -60,58 +44,61 @@ def http_proxy_connect(addr):
 	s.connect((phost,pport))
 
 	s.send(('CONNECT %s:%d HTTP/1.0\r\n' % addr).encode())
-	s.send(('\r\n'.join('%s: %s' % (k, v) for (k, v) in headers.items()) + '\r\n\r\n').encode())
-	print('sent')
+	s.send(('\r\n'.join('%s: %s'%(k,v) for k,v in headers.items()) + '\r\n\r\n').encode())
 
 	statusline = readline(s).strip()
 	version, status, statusmsg = statusline.split(' ', 2)
 	status = int(status)
 	response_headers = {}
-	while True:
+	l = '?'
+	while l:
 		l = readline(s).strip()
-		if l == '':
-			break
-		if not ':' in l:
-			continue
-		k, v = l.split(':', 1)
-		response_headers[k.strip().lower()] = v.strip()
+		if ':' in l:
+			k, v = l.split(':', 1)
+			response_headers[k.strip().lower()] = v.strip()
 	return s, status, response_headers
 
-def runrouter(targetaddr, boundaddr, latency = 0.32, jitter = 0.05, loss_bytes = 0.0, onevent = None):
+def runrouter(targetaddr, boundaddr, latency = 0.32, jitter = 0.05, loss_bytes = 0.0, onevent = lambda e:e):
 	latency, jitter = latency/2, jitter/2	# Half time there, half time home.
-	targetaddr, boundaddr = [(socket.gethostbyname(addr[0]), addr[1]) for addr in [targetaddr, boundaddr]]
+	targetaddr, boundaddr = [(addr[0], addr[1]) for addr in [targetaddr, boundaddr]]
 	sock = socket.socket()	# TCP
 	sock.bind(boundaddr)
 	sock.listen(5)
 	while True:
 		inbound_sock,iaddr = sock.accept()
-		Thread(target=handle_route, args=(inbound_sock, targetaddr, latency, jitter, loss_bytes, onevent)).start()
+		Thread(target=handle_route, args=(inbound_sock, targetaddr, boundaddr, latency, jitter, loss_bytes, onevent)).start()
 
 
-def handle_route(inbound_sock, targetaddr, latency, jitter, loss_bytes, onevent):
-	# try:
-	onevent('i')
-	scheduler = EternalScheduler()
-	outbound_sock,_,_ = http_proxy_connect(targetaddr)
-	onevent('o')
-	socks = [inbound_sock, outbound_sock]
-	print(socks)
-	while True:
-		rs,_,xs = select(socks, [], socks)
-		if xs:
-			break
-		for r in rs:
-			w = socks[socks.index(r)^1]
-			data = r.recv(1)
-			if uniform(0.0, 100.0) < loss_bytes:
-				onevent('d')
-				continue
-			else:
-				onevent(data)
-			delay = latency + uniform(0.0, jitter)
-			scheduler.enter(delay, w.send, (data,))
-	# except Exception as e:
-		# onevent(e)
+def handle_route(inbound_sock, targetaddr, boundaddr, latency, jitter, loss_bytes, onevent):
+	try:
+		onevent('i')
+		outbound_sock,_,_ = http_proxy_connect(targetaddr)
+		onevent('o')
+		socks = [inbound_sock, outbound_sock]
+		[s.setblocking(0) for s in socks]
+		httpfrom, httpto = ('\r\nHost: %s:%i\r\n' % boundaddr).encode(), ('\r\nHost: %s:%i\r\n' % targetaddr).encode()
+		quit = False
+		while not quit:
+			rs,_,xs = select(socks, [], socks)
+			if xs:
+				break
+			for r in rs:
+				sleep(latency + uniform(0.0, jitter))
+				w = socks[socks.index(r)^1]
+				data = r.recv(256*1024)
+				if not data:
+					quit = True
+					break
+				data = data.replace(httpfrom, httpto)
+				if uniform(0.0, 100.0) < loss_bytes:
+					onevent('d')
+					continue
+				else:
+					onevent('.')
+				w.send(data)
+	except Exception as e:
+		onevent(e)
+	onevent('c')
 
 
 def main():
@@ -121,7 +108,7 @@ def main():
 		print('Example: %s somedomain.com:80 localhost:8080 1.5 0.5 0.0' % sys.argv[0])
 		sys.exit(1)
 	targetaddr, boundaddr = [x.split(':') for x in sys.argv[1:3]]
-	targetaddr, boundaddr = [(socket.gethostbyname(addr[0]), int(addr[1])) for addr in [targetaddr, boundaddr]]
+	targetaddr, boundaddr = [(addr[0], int(addr[1])) for addr in [targetaddr, boundaddr]]
 	latency, jitter, loss_bytes = [float(x) for x in sys.argv[3:6]]
 	def onevent(e):
 		if type(e) == str:
@@ -134,7 +121,7 @@ def main():
 	print('Latency setting:        %g' % latency)
 	print('Jitter setting:         %g' % jitter)
 	print('Byte loss setting:      %g%%' % loss_bytes)
-	print('i means incoming connect, o outgoing connect, . packet routed, d means packet dropped.')
+	print('i=incoming connect, o=outgoing connect, c=connection closed, .=packet routed, d=packet dropped.')
 	runrouter(targetaddr, boundaddr, latency, jitter, loss_bytes, onevent)
 
 
